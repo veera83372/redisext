967f723bdca4d8aee73242ceb4c40ee1d94d7a8f
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2020-09-05 12:50:02 +0300

    Fix hook

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/go-redis/redisext
+
+go 1.13
+
+require (
+	github.com/go-redis/redis/v8 v8.0.0-beta.10
+	go.opentelemetry.io/otel v0.11.0
+)
--- otel.go
+++ otel.go
@@ -2,10 +2,15 @@ package redisext
 
 import (
 	"context"
+	"encoding/hex"
+	"fmt"
+	"strconv"
 	"strings"
+	"time"
+	"unicode/utf8"
 
 	"github.com/go-redis/redis/v8"
-	"github.com/go-redis/redis/v8/internal"
+	"github.com/go-redis/redisext/internal"
 	"go.opentelemetry.io/otel/api/global"
 	"go.opentelemetry.io/otel/api/trace"
 	"go.opentelemetry.io/otel/label"
@@ -36,7 +41,7 @@ func (OpenTelemetryHook) BeforeProcess(ctx context.Context, cmd redis.Cmder) (co
 func (OpenTelemetryHook) AfterProcess(ctx context.Context, cmd redis.Cmder) error {
 	span := trace.SpanFromContext(ctx)
 	if err := cmd.Err(); err != nil {
-		_ = internal.RecordError(ctx, err)
+		span.RecordError(ctx, err)
 	}
 	span.End()
 	return nil
@@ -90,7 +95,7 @@ func (OpenTelemetryHook) BeforeProcessPipeline(ctx context.Context, cmds []redis
 func (OpenTelemetryHook) AfterProcessPipeline(ctx context.Context, cmds []redis.Cmder) error {
 	span := trace.SpanFromContext(ctx)
 	if err := cmds[0].Err(); err != nil {
-		_ = internal.RecordError(ctx, err)
+		span.RecordError(ctx, err)
 	}
 	span.End()
 	return nil
@@ -105,7 +110,7 @@ func appendCmd(b []byte, cmd redis.Cmder) []byte {
 		}
 
 		start := len(b)
-		b = internal.AppendArg(b, arg)
+		b = appendArg(b, arg)
 		if len(b)-start > lenLimit {
 			b = append(b[:start+lenLimit], "..."...)
 		}
@@ -117,4 +122,59 @@ func appendCmd(b []byte, cmd redis.Cmder) []byte {
 	}
 
 	return b
+}
+
+func appendArg(b []byte, v interface{}) []byte {
+	switch v := v.(type) {
+	case nil:
+		return append(b, "<nil>"...)
+	case string:
+		return appendUTF8String(b, internal.Bytes(v))
+	case []byte:
+		return appendUTF8String(b, v)
+	case int:
+		return strconv.AppendInt(b, int64(v), 10)
+	case int8:
+		return strconv.AppendInt(b, int64(v), 10)
+	case int16:
+		return strconv.AppendInt(b, int64(v), 10)
+	case int32:
+		return strconv.AppendInt(b, int64(v), 10)
+	case int64:
+		return strconv.AppendInt(b, v, 10)
+	case uint:
+		return strconv.AppendUint(b, uint64(v), 10)
+	case uint8:
+		return strconv.AppendUint(b, uint64(v), 10)
+	case uint16:
+		return strconv.AppendUint(b, uint64(v), 10)
+	case uint32:
+		return strconv.AppendUint(b, uint64(v), 10)
+	case uint64:
+		return strconv.AppendUint(b, v, 10)
+	case float32:
+		return strconv.AppendFloat(b, float64(v), 'f', -1, 64)
+	case float64:
+		return strconv.AppendFloat(b, v, 'f', -1, 64)
+	case bool:
+		if v {
+			return append(b, "true"...)
+		}
+		return append(b, "false"...)
+	case time.Time:
+		return v.AppendFormat(b, time.RFC3339Nano)
+	default:
+		return append(b, fmt.Sprint(v)...)
+	}
+}
+
+func appendUTF8String(dst []byte, src []byte) []byte {
+	if utf8.Valid(src) {
+		return append(dst, src...)
+	}
+
+	s := len(dst)
+	dst = append(dst, make([]byte, hex.EncodedLen(len(src)))...)
+	hex.Encode(dst[s:], src)
+	return dst
 }
